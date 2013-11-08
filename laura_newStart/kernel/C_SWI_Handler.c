5d66311e90ce60667b97dedbc911f6c0a1cb62b6
Laura <lbloch220@yahoo.com>
2013-11-08 21:08:46 +0100

    working kernel, progress check 3.5

---
+++ laura_newStart/kernel/C_SWI_Handler.c
@@ -0,0 +1,32 @@
+#include "S_Handler.h"
+#include <exports.h>
+#include <bits/swi.h>
+
+void exit(int status);
+ssize_t read(int fd, void* buf, size_t count);
+ssize_t write(int fd, void* buf, size_t count);
+
+
+void C_SWI_Handler(unsigned swi_num, unsigned *regs)
+{
+	switch (swi_num){
+		case  EXIT_SWI: 
+			exit(*regs);
+			break;
+		case  READ_SWI: 
+			regs[0] = read((int)regs[0], (void*)regs[1], (size_t)regs[2]);
+			break;
+		case  WRITE_SWI: 
+			regs[0] = write((int)regs[0], (void*)regs[1], (size_t)regs[2]);
+			break;
+/*		case TIME_SWI:
+			regs[0] = time();
+			break;
+		case SLEEP_SWI:
+			regs[0] = sleep((int)regs[0]);
+			break;
+*/		default: 
+			exit(-1); 
+			 break;
+	}
+}
---
+++ laura_newStart/kernel/read.c
@@ -0,0 +1,59 @@
+#include "include/bits/errno.h"
+#include "include/bits/swi.h"
+#include "include/bits/fileno.h"
+#include <exports.h>
+
+ssize_t read(int fd, void* buf, size_t c){                                  
+
+  char *buffer = (char*) buf;
+  int i = 0;
+  char newChar;
+  int count = (int) c;
+                                                                              
+  int SDRAM_start = 0xa0000000;                                                 
+  int SDRAM_end = 0xa3ffffff;                               
+  int SDRAM_size = SDRAM_end - SDRAM_start;                                     
+                                                                       
+  //Case 1: If fd doesn't match stdin                       
+  if(fd != STDIN_FILENO) return EBADF;                                          
+                                                            
+  if((count > SDRAM_size) || (buf < (void*)SDRAM_start) || (buf > (void*)SDRAM_end))
+     return EFAULT;                                                             
+   
+  //Case 3: read into buffer                                    
+  for(i = 0; i < count; i++){                                                   
+	newChar = getc();                                                           
+    //EOT character                                                             
+    if(newChar == 4){                                                       
+      return i;                                                                 
+    }                                                                           
+                                                                                
+    //Backspace or delete character                                             
+    else if((newChar == '\b') || (newChar == 127)){                             
+        if(i > 0) {                                                             
+          puts("\b \b");                                                        
+          i -= 2;                                                               
+          buffer[i+1] = 0;                                                      
+        }                                                                       
+        else {                                                                  
+          i -= 1;                                                               
+        }                                                                       
+    }                                                                           
+                                                                                
+    //Newline or Carriage return characters                                     
+    else if((newChar == '\n') || (newChar =='\r')){                
+     buffer[i] = '\n';                                                         
+      putc('\n');                                                               
+      return i;                                                                 
+    }                                                                           
+                                                                                
+    //Regular characters                                                        
+    else{                                                                       
+      buffer[i] = newChar;                                                      
+      putc(newChar);                                                            
+    }                                                                           
+  }                         
+  return count;                                                                 
+}                                                                               
+                                                                                
+                              
